71b5a6fae0bdad1eac3d62c3184a6e2f79024fb0
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-03-04 17:26:55 -0500

    reworking of chisq fitting, added likelihood fitting, rename n->density g0->radiation_field, more header fixing

--- lineratiofit.py
+++ lineratiofit.py
@@ -53,7 +53,8 @@ class LineRatioFit(Tool):
         self._chisq = None
         self._deltasq = None
         self._reducedChisq = None
-        self.isrf_unit = None
+        self._likelihood = None
+        self.radiation_field_unit = None
         self.density_unit = None
         self._plotter = LineRatioPlot(self)
     
@@ -225,15 +226,16 @@ class LineRatioFit(Tool):
             self._modelratios[k] = CCDData.read(thefile,unit=unit)
             if True:
                 self._modelratios[k].header["CUNIT1"] = "cm-3"
-                self._modelratios[k].header["CUNIT2"] = "erg/(cm2 s)"
+                #self._modelratios[k].header["CUNIT2"] = "erg cm-2 s-1"
+                self._modelratios[k].header["CUNIT2"] = "Habing"
             if not self.density_unit:
                 try:
                     self.density_unit = u.Unit(self._modelratios[k].header["CUNIT1"])
                 except KeyError:
                     raise Exception("Keyword CUNIT1 is required in file %s FITS header to describe units of density" % thefile)
-            if not self.isrf_unit:
+            if not self.radiation_field_unit:
                 try:
-                    self.isrf_unit    = u.Unit(self._modelratios[k].header["CUNIT2"])
+                    self.radiation_field_unit    = u.Unit(self._modelratios[k].header["CUNIT2"])
                 except KeyError:
                     raise Exception("Keyword CUNIT2 is required in file %s FITS header to describe units of interstellar radiation field"%thefile)
     
@@ -244,9 +246,10 @@ class LineRatioFit(Tool):
         self.computeDeltaSqMap()
         self.computeChisq()
         self.writeChisq()
-        self.computeBestnG0Maps()
+        self.computeBest_density_radiation_field_Maps()
      
     def observedratios(self):
+        '''Returns a list of the observed line ratios that have been input so far'''
         return list(self._observedratios.keys())
 
     def computeValidRatios(self):
@@ -270,14 +273,14 @@ class LineRatioFit(Tool):
         if "CII_158" in m and "FIR" in m:
             if "OI_63" in m:
                 l="OI_63+CII_158/FIR"
-                print(l)
+                #print(l)
                 a = deepcopy(self._measurements["OI_63"])+deepcopy(self._measurements["CII_158"])
                 b = deepcopy(self._measurements["FIR"])
                 self._observedratios[l] = a/b
                 self._ratioHeader("OI_63+CII_158","FIR",l)
             if "OI_145" in m:
                 ll="OI_145+CII_158/FIR"
-                print(ll)
+                #print(ll)
                 aa = deepcopy(self._measurements["OI_145"])+deepcopy(self._measurements["CII_158"])
                 bb = deepcopy(self._measurements["FIR"])
                 self._observedratios[ll] = aa/bb
@@ -298,7 +301,19 @@ class LineRatioFit(Tool):
             self._deltasq[r] = _q.multiply(_q)
 
     def computeDeltaSqMap(self):
-        '''Compute the difference-squared values from the observed ratios and models - multi-pixel version'''
+        '''Compute the difference-squared values from the observed ratios 
+           and models - multi-pixel version and store in _deltasq member'''
+        self._deltasq = self._computeDelta(f=0)
+
+    def _computeDelta(self,f):
+        '''Compute the difference-squared values from the observed ratios 
+           and models - multi-pixel version
+           Parameters:
+                f - fractional amount by which the variance is underestimated. 
+                    For traditional chi-squared calculation f is zero.  
+                    For log-likelihood calculation f is positive and less than 1.
+                    See, e.g. https://emcee.readthedocs.io/en/stable/tutorials/line/#maximum-likelihood-estimation
+        '''
         #@todo perhaps don't store _modelratios but have them be a return value of read_fits.
         # reasoning is that if we use the same PDRUtils object to make multiple computations, we
         # have to be careful to clear _modelratios each time.
@@ -312,7 +327,7 @@ class LineRatioFit(Tool):
         if not self._check_ratio_shapes():
             raise TypeError("Observed ratio maps have different dimensions")
             
-        self._deltasq = dict()
+        returnval = dict()
         for r in self._observedratios:
             sz = self._modelratios[r].size
             _z = np.reshape(self._modelratios[r],sz)
@@ -320,10 +335,21 @@ class LineRatioFit(Tool):
             ff = list()
             for pix in _z:
                 mf = ma.masked_invalid(self._observedratios[r].flux)
-                me = ma.masked_invalid(self._observedratios[r].error)
+                me = ma.masked_invalid(self._observedratios[r].error)  
+                #optional fractional error correction for log likelihood.
+                #
+                if f == 0:
+                    s2 = me**2
+                    add_term = 0
+                else:
+                   #term is actually log(2*pi*s2) but addition of 
+                   #constant makes no difference in likelihood.
+                    frac_error  = f*pix
+                    s2 = me**2+frac_error**2
+                    add_term = np.log(s2)
                 #_q = (self._observedratios[r].flux - pix)/self._observedratios[r].error
-                _q = (mf - pix)/me
-                ff.append(_q*_q)
+                _q = (mf - pix)**2/s2 + add_term
+                ff.append(_q)
             # result order is g0,n,y,x
             newshape = np.hstack((self._modelratios[r].shape,self._observedratios[r].shape))
             # result order is y,x,g0,n
@@ -331,8 +357,25 @@ class LineRatioFit(Tool):
             _qq = np.reshape(ff,newshape)
             _wcs = self._observedratios[r].wcs.deepcopy()
             _meta= deepcopy(self._observedratios[r].meta)
-            self._deltasq[r] = CCDData(_qq,unit="adu",wcs=_wcs,meta=_meta)
-           
+            returnval[r] = CCDData(_qq,unit="adu",wcs=_wcs,meta=_meta)
+    
+        return returnval
+
+    def computeLogLikelihood(self,f):
+        l = self._computeDelta(f)
+        sumary = -0.5* sum((l[r]._data for r in l))
+        k = utils.firstkey(self._deltasq)
+        self._likelihood = CCDData(sumary,unit='adu',wcs=self._deltasq[k].wcs,meta=self._deltasq[k].meta)
+        self._fixheader(self._likelihood)
+        self._makehistory(self._likelihood)
+
+
+#    def log_likelihood(self, theta):
+#       ratio, log_f = theta
+#       l = self._likelihood(f=np.exp(log_f))
+#       sumary = -0.5* sum((self._likelihood[r]._data for r in self._likelihood))
+#       return sumary
+
     def computeChisq(self):
         '''Compute the chi-squared values from observed ratios and models'''
         if self.ratiocount < 2 :
@@ -351,20 +394,19 @@ class LineRatioFit(Tool):
         
     def writeChisq(self,file="chisq.fits",rfile="rchisq.fits"):
         '''Write the chisq and reduced-chisq data to a file'''
-        self._chisq.write(file,overwrite=True)
-        self._reducedChisq.write(rfile,overwrite=True)  
-        
-    def computeBestnG0Maps(self):
+        self._chisq.write(file,overwrite=True,hdu_mask='MASK')
+        self._reducedChisq.write(rfile,overwrite=True,hdu_mask='MASK')  
+
+    def computeLikeliest_density_radiation_field_Maps(self):
         '''Compute the best-fit density n and radiation field G0 spatial maps by searching for the minimum chisq at each spatial pixel.'''
-        if self._chisq is None or self._reducedChisq is None: return
+        if self._likelihood is None: return
         
-        # get the chisq minima of each pixel along the g,n axes
-        z=np.amin(self._reducedChisq,(0,1))
-        #qq=np.transpose(np.vstack(np.where(self._reducedChisq==z)))
-        gi,ni,yi,xi=np.where(self._reducedChisq==z)
+        # get the likelihood maxima of each pixel along the g,n axes
+        z=np.amax(self._likelihood,(0,1))
+        gi,ni,yi,xi=np.where(self._likelihood==z)
         #print(gi)
         #print(len(gi),len(ni),len(xi),len(yi))
-        spatial_idx = [yi,xi]
+        spatial_idx = (yi,xi)
         model_idx   = np.transpose(np.array([ni,gi]))
         # qq[:,:2] takes the first two columns of qq
         # [:,[1,0]] swaps those columns
@@ -379,17 +421,67 @@ class LineRatioFit(Tool):
         #n =10**(self._modelratios[fk].wcs.wcs_pix2world(np.flip(qq[:,:2]),0))[:,0].reshape(newshape)
         g0=10**(self._modelratios[fk].wcs.wcs_pix2world(model_idx,0))[:,1]
         n =10**(self._modelratios[fk].wcs.wcs_pix2world(model_idx,0))[:,0]
-        print("G0 shape ",g0.shape)
-        print("N shape ",n.shape)
-        self.g0_map=self._observedratios[fk2].copy()
-        self.g0_map.data[spatial_idx]=g0
-        self.g0_map.unit = self.isrf_unit
-        self.n_map=self._observedratios[fk2].copy()      
-        self.n_map.data[spatial_idx]=n
-        self.n_map.unit = self.density_unit
+        #print("G0 shape ",g0.shape)
+        #print("N shape ",n.shape)
+        self.L_radiation_field=self._observedratios[fk2].copy()
+        self.L_radiation_field.data[spatial_idx]=g0
+        self.L_radiation_field.unit = self.radiation_field_unit
+        self.L_radiation_field.uncertainty.unit = self.radiation_field_unit
+        self.L_density=self._observedratios[fk2].copy()      
+        self.L_density.data[spatial_idx]=n
+        self.L_density.unit = self.density_unit
+        self.L_density.uncertainty.unit = self.density_unit
         #fix the headers
-        self._nG0header() 
+        #self._density_radiation_field_header() 
         
+    def computeBest_density_radiation_field_Maps(self):
+        '''Compute the best-fit density n and radiation field G0 spatial maps by searching for the minimum chisq at each spatial pixel.'''
+        if self._chisq is None or self._reducedChisq is None: return
+        
+        # get the chisq minima of each pixel along the g,n axes
+        z=np.amin(self._reducedChisq,(0,1))
+        #qq=np.transpose(np.vstack(np.where(self._reducedChisq==z)))
+        gi,ni,yi,xi=np.where(self._reducedChisq==z)
+        #print(gi)
+        #print(len(gi),len(ni),len(xi),len(yi))
+        spatial_idx = (yi,xi)
+        model_idx   = np.transpose(np.array([ni,gi]))
+        # qq[:,:2] takes the first two columns of qq
+        # [:,[1,0]] swaps those columns
+        # np.flip would also swap them.
+        #print(qq[:,:2][:,[1,0]])
+        #print(np.flip(qq[:,:2]))
+        fk = utils.firstkey(self._modelratios)
+        fk2 = utils.firstkey(self._observedratios)
+        newshape = self._observedratios[fk2].shape
+        # figure out which G0,n the minimum refer to, and reshape into the RA-DEC map
+        #g0=10**(self._modelratios[fk].wcs.wcs_pix2world(np.flip(qq[:,:2]),0))[:,1].reshape(newshape)
+        #n =10**(self._modelratios[fk].wcs.wcs_pix2world(np.flip(qq[:,:2]),0))[:,0].reshape(newshape)
+        g0=10**(self._modelratios[fk].wcs.wcs_pix2world(model_idx,0))[:,1]
+        n =10**(self._modelratios[fk].wcs.wcs_pix2world(model_idx,0))[:,0]
+        #print("G0 shape ",g0.shape)
+        #print("N shape ",n.shape)
+
+        # note this will reshape g0 in radiation_field for us!
+        self._radiation_field=self._observedratios[fk2].copy()
+        self._radiation_field.data[spatial_idx]=g0
+
+        # We cannot mask nans because numpy does not support writing
+        # MaskedArrays to a file. Will get a not implemented error.
+        # Therefore just copy the nans over from the input observations.
+        self._radiation_field.data[np.isnan(self._observedratios[fk2])] = np.nan
+
+        self._radiation_field.unit = self.radiation_field_unit
+        self._radiation_field.uncertainty.unit = self.radiation_field_unit
+
+        self._density=self._observedratios[fk2].copy()      
+        self._density.data[spatial_idx]=n
+        self._density.data[np.isnan(self._observedratios[fk2])] = np.nan
+        self._density.unit = self.density_unit
+        self._density.uncertainty.unit = self.density_unit
+
+        #fix the headers
+        self._density_radiation_field_header() 
             
     def comment(self,value,image):
         '''Add a comment to the image header'''
@@ -434,17 +526,27 @@ class LineRatioFit(Tool):
         self._dataminmax(image)
 
     def _ratioHeader(self,numerator,denominator,label):
-        '''Add the RATIO identifier to the appropriate image'''
+        '''Add the RATIO identifier to the appropriate image
+           Parameters:
+                numerator - numerator string key of the line ratio
+                denominator - denominator string key of the line ratio
+                label - ratio label
+        '''
         self.addkey("RATIO",label,self._observedratios[label])
         self._dataminmax(self._observedratios[label])
         self._signature(self._observedratios[label])
         
     def _fixheader(self,image):
-        """Put axis 3 and 4 header values into chisq maps"""
-        self.setkey("CTYPE3","Log(n)",image)
-        self.setkey("CTYPE4","Log(G0)",image)
+        '''Put axis 3 and 4 header values into chisq maps
+           Parameter:
+                image - the image to which to add the header values
+        '''
+        # @TODO make these headers compliant with inputs (e.g. requested units)
+        self.setkey("CTYPE3","Log(Volume Density)",image)
+        self.setkey("CTYPE4","Log(Radiation Field)",image)
         self.setkey("CUNIT3",str(self.density_unit),image)
-        self.setkey("CUNIT4",str(self.isrf_unit),image)
+        self.setkey("CUNIT4",str(self.radiation_field_unit),image)
+        #@TODO this cdelts will change with new models.  make this flexible
         self.setkey("CDELT3",0.25,image)
         self.setkey("CDELT4",0.25,image)
         self.setkey("CRVAL4",-0.5,image)
@@ -453,17 +555,21 @@ class LineRatioFit(Tool):
         self.setkey("CRPIX4",1.0,image)
         
          
-    def _nG0header(self):
-        '''Common header items in the n and G0 FITS files'''
-        self.n_map.header.pop('RATIO')
-        self.g0_map.header.pop('RATIO')
-        self.setkey("BUNIT",self.density_unit,self.n_map)
-        self.comment("Best-fit H2 volume density",self.n_map)
-        self.setkey("BUNIT",self.isrf_unit,self.g0_map)
-        self.comment("Best-fit interstellar radiation field",self.g0_map)
-        self._makehistory(self.n_map)
-        self._makehistory(self.g0_map)
-              
+    def _density_radiation_field_header(self):
+        '''Common header items in the density and radiation field FITS files'''
+        self._density.header.pop('RATIO')
+        self._radiation_field.header.pop('RATIO')
+        # note: must use to_string() here or astropy.io.fits.Card complains
+        # about the value being a Unit.  Oddly it doesn't complain for the
+        # data units.  Go figure.
+        self.setkey("BUNIT",self.density_unit.to_string(),self._density)
+        self.comment("Best-fit H2 volume density",self._density)
+        self.setkey("BUNIT",self.radiation_field_unit.to_string(),self._radiation_field)
+        self.comment("Best-fit interstellar radiation field",self._radiation_field)
+        self._makehistory(self._density)
+        self._makehistory(self._radiation_field)
+
+       
 
 if __name__ == "__main__":
     from measurement import Measurement 
