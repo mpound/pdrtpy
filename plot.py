2ebd30fe62de0e039ce99da563a9b46b948c15ea
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-03-04 17:22:18 -0500

    better plotting

--- plot.py
+++ plot.py
@@ -11,6 +11,8 @@ import matplotlib.colors as mpcolors
 import matplotlib.cm as mcm
 from matplotlib import ticker
 from matplotlib.lines import Line2D
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.axes as maxes
 
 from astropy.nddata.utils import Cutout2D
 from astropy.io import fits
@@ -19,6 +21,7 @@ import astropy.units as u
 from astropy.nddata import NDDataArray, CCDData, NDUncertainty, StdDevUncertainty, VarianceUncertainty, InverseVariance
 from astropy.visualization import simple_norm, ZScaleInterval , ImageNormalize
 from astropy.visualization.stretch import SinhStretch,  LinearStretch
+from matplotlib.colors import LogNorm
 
 import pdrutils as utils
 
@@ -31,6 +34,8 @@ class PlotBase:
     def __init__(self,tool):
         import matplotlib.pyplot as plt
         self._plt = matplotlib.pyplot
+        self._figure = None
+        self._axis = None
         self._tool = tool
         #print("Done PlotBase")
 
@@ -38,10 +43,10 @@ class PlotBase:
         # tip of the hat to the WIP autolevels code lev.
         # http://admit.astro.umd.edu/wip ,  wip/src/plot/levels.c
         # CVS at http://www.astro.umd.edu/~teuben/miriad/install.html
-        print(type(data))
+        #print(type(data))
         max_ =data.max()
         min_ = data.min()
-        print("autolev min %f max %f"%(min_,max_))
+        #print("autolev min %f max %f"%(min_,max_))
         if numlevels is None:
             numlevels = int(0.5+3*(np.log(max_)-np.log(min_))/np.log(10))
         #print("levels start %d levels"%numlevels)
@@ -60,19 +65,41 @@ class PlotBase:
             levels = np.array([min_ * np.power(10,slope*j) for j in range(0,numlevels)])
         else:
            raise Exception("steps must be 'lin' or 'log'")
-        print("Autolevels got %d levels %s"%(numlevels,levels))
+        print("Computed %d contour autolevels: %s"%(numlevels,levels))
         return levels
         
     def _zscale(self,image):
-        norm= ImageNormalize(image,ZScaleInterval(contrast=0.5),stretch=LinearStretch())
+        # clip=False required or NaNs get max color value, see https://github.com/astropy/astropy/issues/8165
+        norm= ImageNormalize(data=image,interval=ZScaleInterval(contrast=0.5),stretch=LinearStretch(),clip=False)
         return norm
-    
+
+    def _wcs_colorbar(self,image, axis, pos="right", width="10%",pad=0.15,orientation="vertical"):
+        """Create a colorbar for a subplot with WCSAxes 
+           (as opposed to matplolib Axes).  There are some side-effects of
+           using WCS procjection that need to be ameliorated.  Also for 
+           subplots, we want the colorbars to have the same height as the 
+           plot, which is not the default behavior.
+
+           Parameters:
+              image - the mappable object for the plot. Must not be masked.
+              axis - which Axes object for the plot
+              pos - colorbar position: ["left"|"right"|"bottom"|"top"]. Default: right
+              width - string, width of the colorbar in terms of percent width of the plot.
+              pad - padding between colorbar and plot, in inches.
+              orientation - string, ["vertical" | "horizontal" ]
+        """
+        divider = make_axes_locatable(axis)
+        # See https://stackoverflow.com/questions/47060939/matplotlib-colorbar-and-wcs-projection
+        cax = divider.append_axes(pos, size=width, pad=pad, axes_class=maxes.Axes)
+        cax.yaxis.set_ticks_position(pos)
+        return self._figure.colorbar(image,ax=axis,cax=cax)
+
 class LineRatioPlot(PlotBase):
     """ Class to plot various results from PDR Toolbox model fitting """
     def __init__(self,tool,**kwargs):
         super().__init__(tool)
-        self.figure = None
-        self.axis = None
+        self._figure = None
+        self._axis = None
         self._xlim = [None,None]
         self._ylim = [None,None]
         self._plotkwargs = kwargs
@@ -88,53 +115,74 @@ class LineRatioPlot(PlotBase):
     def observedratio(self,identifier,image=True,contours=False,levels=None):
         self._plot(data=self._tool._observedratios[identifier],units=u.dimensionless_unscaled,cmap='viridis',image=image,contours=contours,levels=levels,norm=None,title=identifier)
 
-    def density(self,units='cm^-3',cmap='plasma',image=True, contours=False,levels=None,norm=None):
-        #fancyunits=self._tool.n_map.unit.to_string('latex')
+    def density(self,units='cm^-3',cmap='plasma',image=True, contours=False,levels=None,norm=None,**kwargs):
+        #fancyunits=self._tool._density.unit.to_string('latex')
         fancyunits=u.Unit(units).to_string('latex')
         _title = 'n ('+fancyunits+')'
-        self._plot(self._tool.n_map,units,cmap,image,contours,levels,norm,title=_title)
+        self._plot(self._tool._density,units,cmap,image,contours,levels,norm,title=_title,**kwargs)
 
-    def radiationField(self,units='Habing',cmap='plasma',image=True, contours=False,levels=None,norm=None):
-        #fancyunits=self._tool.g0_map.unit.to_string('latex')
+    def radiationField(self,units='Habing',cmap='plasma',image=True, contours=False,levels=None,norm=None,**kwargs):
+        #fancyunits=self._tool._radiation_field.unit.to_string('latex')
         if units not in rad_title:
             fancyunits=u.Unit(units).to_string('latex')
             _title='Radiation Field ('+fancyunits+')'
         else:
             _title=rad_title[units]
-        self._plot(self._tool.g0_map,units,cmap,image,contours,levels,norm,title=_title)
+        self._plot(self._tool._radiation_field,units,cmap,image,contours,levels,norm,title=_title,**kwargs)
+
+    def plot_both(self,units=['Habing','cm^-3'],cmap='plasma',image=True, contours=False,levels=None,norm=None):
+       self.radiationField(units=units[0],cmap=cmap,image=image,contours=contours,levels=levels,norm=norm,nrows=1,ncols=2,index=1,reset=True)
+       self.density(units=units[1],cmap=cmap,image=image,contours=contours,levels=levels,norm=norm,nrows=1,ncols=2,index=2,reset=False)
 
     def _plot(self,data,units,cmap,image,contours,levels,norm,title,**kwargs):
+        #@Todo raise exceptions 
+        #@Todo separate imshow kwargs from other kwargs. See e.g.  https://fermipy.readthedocs.io/en/latest/_modules/fermipy/plotting.html
         k=utils.to(units,data)
         km = ma.masked_invalid(k)
-        min_ = km.min()
-        max_ = km.max()
-        ax=self._plt.subplot(111,projection=k.wcs,aspect='equal')
+        # make sure nans don't affect the color map
+        min_ = np.nanmin(km)
+        max_ = np.nanmax(km)
+        #print(kwargs)
+        nrows = kwargs.get("nrows",1)
+        ncols = kwargs.get("ncols",1)
+        index = kwargs.get("index",1)
+        reset = kwargs.get("reset",True)
+        figsize=kwargs.get("figsize",(ncols*5,ncols*5))
+        #print(nrows,ncols,index,reset,figsize)
+        axidx = index-1
+        if reset:
+            self._figure,self._axis = self._plt.subplots(nrows,ncols,figsize=figsize,subplot_kw={'projection':k.wcs,'aspect':'auto'},constrained_layout=True)
+        # Make sure self._axis is an array because we will index it below.
+        if type(self._axis) is not np.ndarray:
+            self._axis = np.array([self._axis])
         if norm == 'simple':
             normalizer = simple_norm(km, min_cut=min_,max_cut=max_, stretch='log', clip=False)
         elif norm == 'zscale':
             normalizer = self._zscale(km)
+        elif norm == 'log':
+            normalizer = LogNorm()
         else: 
             normalizer = norm
         
         if image:
             current_cmap = mcm.get_cmap(cmap)
             current_cmap.set_bad(color='white',alpha=1)
-            self._plt.imshow(km,origin='lower',norm=normalizer,cmap=cmap)
-            self._plt.colorbar()
+            im=self._axis[axidx].imshow(km,origin='lower',norm=normalizer,cmap=cmap)
+            self._wcs_colorbar(im,self._axis[axidx])
         if contours:
             if image==False: colors='black'
             else: colors='white'
             if levels is None:
                 # Figure out some autolevels 
                 steps='log'
-                contourset = ax.contour(km, levels=self._autolevels(km,steps),colors=colors)
+                contourset = self._axis[axidx].contour(km, levels=self._autolevels(km,steps),colors=colors)
             else:
-                contourset = ax.contour(km, levels=levels, colors=colors)
+                contourset = self._axis[axidx].contour(km, levels=levels, colors=colors)
                 # todo: add contour level labelling
                 # See https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/contour_label_demo.html
-        if title is not None: self._plt.title(title)
-        self._plt.xlabel(k.wcs.wcs.lngtyp)
-        self._plt.ylabel(k.wcs.wcs.lattyp)
+        if title is not None: self._axis[axidx].set_title(title)
+        self._axis[axidx].set_xlabel(k.wcs.wcs.lngtyp)
+        self._axis[axidx].set_ylabel(k.wcs.wcs.lattyp)
         
 ######################################################3
 ## FROM HERE DOWN NEEDS WORK FOR MULTIPIXEL MAPS
