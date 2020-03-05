3fe8da41b0f06fb27130db9692f2cfcaf2759bf1
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-03-05 11:04:24 -0500

    use tabrow, add supported_*, list, and easy access models

--- modelset.py
+++ modelset.py
@@ -2,7 +2,6 @@ import pdrutils as utils
 import numpy as np
 
 #@ToDo:
-#   model version should be a property
 #   addModelSet() - for custom model sets. See model convention white paper
 class ModelSet(object):
     """Class for PDR Model Sets. Model Sets will interface with a  
@@ -11,40 +10,34 @@ class ModelSet(object):
           name - string identifier
           z    - metallicity in solar units.  
     """
-       
     def __init__(self,name,z):
         self._all_models = utils.get_table("all_models.tab")
         self._all_models.add_index("name")
         if name not in self._all_models["name"]:
             raise ValueError("Unrecognized model %s. Choices are: %s"%(name,self._possible_models))
-        self._name = name
 
         zzz = self._all_models.loc[name]["z"]
         self._row = np.where(zzz==z)
-        if self._row.size == 0:
+        if self._row[0].size == 0:
             raise ValueError("Z=%2.1f not found in %s. Choices are: %s"%(z,name,zzz))
-        self._metallicity = z
         self._row = self._row[0][0]
-        # maybe I should just keep tabrow around and not make all these member variables.
-        tabrow = self._all_models[self._row]
-        self._version = tabrow["version"]
-        self._filename = tabrow["filename"]
-        self._table = utils.get_table(path=tabrow["path"],filename=tabrow["filename"])
+        self._tabrow = self._all_models[self._row]
+        self._table = utils.get_table(path=self._tabrow["path"],filename=self._tabrow["filename"])
 
     @property
     def description(self):
         """Return the description of this model"""
-        return self._all_models.loc[self._name][self._row]["description"]+", Z=%2.1f"%self.z
+        return self._tabrow["description"]+", Z=%2.1f"%self.z
 
     @property
     def name(self):
         """Return the name of this model"""
-        return self._name
+        return self._tabrow["name"]
 
     @property
     def version (self):
         """Return the name of this model"""
-        return self._version
+        return self._tabrow["version"]
 
     @property
     def z(self):
@@ -54,7 +47,7 @@ class ModelSet(object):
     @property
     def metallicity(self):
         """Return the metallicity of this model"""
-        return self._metallicity
+        return self._tabrow["z"]
 
     #@property
     #def isSolarMetallicity(self):
@@ -64,18 +57,38 @@ class ModelSet(object):
     def table(self):
         """Return an astropy Table containing details of the models"""
         return self._table
-    
 
-#class ModelSet(ModelSetBase)
-#    """PDR Model Sets Model Sets interface with a  
-#       directory containing the model FITS files.
-#       Parameters  
-#          name - string identifier
-#          z    - metallicity in solar units.  Default: 1 (==solar)
-#    """
-#    def __init__(self,name,z=1):
-#        super(name,z)
-#        self._table = utils.get_table(self.all_models[name])
+    @property
+    def supported_lines(self):
+        '''Return a `set` of lines and continuum that are covered by this ModelSet'''
+        #TODO see below
+        return set(np.append(self._table["numerator"].data,self._table["denominator"].data))
+
+    @property
+    def supported_ratios(self):
+       '''Return the emission ratios that are covered by this model'''
+       #TODO: keep as Table column or as something else. Should be consistent with return type of supported_lines
+       return self.table["label"]
+
+    # ============= Static Methods =============
+    @staticmethod
+    def list():
+        """List the names and descriptions of available models"""
+        t = utils.get_table("all_models.tab")
+        t.remove_column("path")
+        t.remove_column("filename")
+        t.pprint_all(align="<")
+
+    @staticmethod
+    def WolfireKaufman():
+    #ToDo later upgrade to variable z when 2020 models are available
+        """Easy way to get the latest Wolfire-Kaufman models, z=1"""
+        return ModelSet("wk2006",z=1)
+
+    @staticmethod
+    def KosmaTau():
+        """Easy way to get the latest KOSMA TAU models, z=1"""
+        return ModelSet("kosmatau",z=1)
 #
 #    def has_metallicity(self,z):
 #        """Check if this model set contains a given metallicity 
