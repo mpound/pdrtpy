f0a453e8a966d84bc4751169574c5453f07df1e4
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-03-10 16:12:53 -0400

    making pip/setup work

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="Photodissociation Region Toolbox",
+    version="2.0b",
+    author = "Marc W. Pound",
+    author_email  = "mpound@umd.edu",
+    description="Photodissociation region analysis tools",
+    keywords="PDR photodissociation",
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires = [
+        'astropy',
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    url="https://dustem.astro.umd.edu",
+    project_urls={
+        "Documentation": "https://pdrtpy.readthedocs.io",
+        "Source Code": "https://github.com/mpound/pdrtpy",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Intended Audience :: Science/Research",
+    ],
+    license = "GPLv3",
+    zip_safe=False,
+)
