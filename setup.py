b58418fba3994f076477eaadba0a43e444453fab
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-05-01 13:03:35 -0400

    duh

--- setup.py
+++ setup.py
@@ -11,38 +11,37 @@ excludelist= ["models","tables","notebooks","testdata","build","dist"]
 print("Found packages ",find_packages(exclude=excludelist))
 
 
-if False:
 
-    setup(
-        name="pdrtpy",
-        version="2.0b",
-        author = "Marc W. Pound",
-        author_email  = "mpound@umd.edu",
-        description="Photodissociation region analysis tools",
-        keywords="PDR photodissociation",
-        long_description=readme(),
-        packages=find_packages(exclude=excludelist)
-        include_package_data = True,
-        package_data = {'pdrtpy': ['tables/*', 'notebooks/*', 'models/*','testdata/*'] },
-        install_requires = [
-            'astropy',
-            'numpy',
-            'scipy',
-            'matplotlib',
-        ],
-        url="https://dustem.astro.umd.edu",
-        project_urls={
-            "Documentation": "https://pdrtpy.readthedocs.io",
-            "Source Code": "https://github.com/mpound/pdrtpy",
-        },
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Topic :: Scientific/Engineering :: Astronomy",
-            "Intended Audience :: Science/Research",
-        ],
-        license = "GPLv3",
-        zip_safe=False,
-        python_requires='>=3.6'
-    )
+setup(
+    name="pdrtpy",
+    version="2.0b",
+    author = "Marc W. Pound",
+    author_email  = "mpound@umd.edu",
+    description="Photodissociation region analysis tools",
+    keywords="PDR photodissociation",
+    long_description=readme(),
+    packages=find_packages(exclude=excludelist),
+    include_package_data = True,
+    package_data = {'pdrtpy': ['tables/*', 'notebooks/*', 'models/*','testdata/*'] },
+    install_requires = [
+        'astropy',
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    url="https://dustem.astro.umd.edu",
+    project_urls={
+        "Documentation": "https://pdrtpy.readthedocs.io",
+        "Source Code": "https://github.com/mpound/pdrtpy",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Intended Audience :: Science/Research",
+    ],
+    license = "GPLv3",
+    zip_safe=False,
+    python_requires='>=3.6'
+)
