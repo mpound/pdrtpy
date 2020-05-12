535b2fc31566116d65a23f67c1271c81a2b3bcde
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-05-12 08:27:42 -0400

    package constants

--- pdrtpy/__init__.py
+++ pdrtpy/__init__.py
@@ -1,6 +1,7 @@
-VERSION = "2.0b4"
+VERSION = "2.0b5"
 AUTHORS =  'Marc W. Pound, Mark G. Wolfire'
 DESCRIPTION="PhotoDissociation Region Toolbox (PDRT), astrophysics analysis tools"
 KEYWORDS = "PDR photodissociation"
+
 __all__ = [ "pdrutils", "measurement", "modelset"]
 
