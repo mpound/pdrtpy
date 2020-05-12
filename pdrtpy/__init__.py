32006846c4fe132279226ab0223b701459a7d05e
Marc Pound <22331890+mpound@users.noreply.github.com>
2020-05-12 10:28:22 -0400

    settling on version numbering scheme

--- pdrtpy/__init__.py
+++ pdrtpy/__init__.py
@@ -1,4 +1,4 @@
-VERSION = "2.0b5"
+VERSION = "2.0.1rc1"
 AUTHORS =  'Marc W. Pound, Mark G. Wolfire'
 DESCRIPTION="PhotoDissociation Region Toolbox (PDRT), astrophysics analysis tools"
 KEYWORDS = "PDR photodissociation"
