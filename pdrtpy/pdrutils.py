265d91a4f940c809623b3d11e578c94824530811
Marc Pound <22331890+mpound@users.noreply.github.com>
2021-01-29 16:42:19 -0500

    add comment

--- pdrtpy/pdrutils.py
+++ pdrtpy/pdrutils.py
@@ -18,6 +18,7 @@ from pdrtpy import version
 _RFS_UNIT_ = u.erg/(u.second*u.cm*u.cm)
 _OBS_UNIT_ = u.erg/(u.second*u.cm*u.cm*u.sr)
 _CM = u.Unit("cm")
+_CM2 = u.Unit("cm-2")
 _KKMS = u.Unit("K km s-1")
 
 # ISRF in other units
@@ -326,6 +327,8 @@ def to(unit,image):
      :return: an image with converted values and units
   """
   #print("converting [%s] in %s to [%s]"%(image.unit,image.header["TITLE"],unit))
+  #@todo check out NDDataArray.convert_unit_to
+  # https://docs.astropy.org/en/stable/api/astropy.nddata.NDDataArray.html#astropy.nddata.NDDataArray.convert_unit_to
   value = image.unit.to(unit)
   newmap = deepcopy(image)
   newmap.data = newmap.data * value
