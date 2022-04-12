746fb13c2d076908ec65d9e8bf29b34bccb06f2d
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-04-12 15:03:38 -0400

    oops beta

--- pdrtpy/__init__.py
+++ pdrtpy/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [ "pdrutils", "measurement", "modelset"]
 
-VERSION = "2.2.6"
+VERSION = "2.2.6b"
 AUTHORS =  'Marc W. Pound, Mark G. Wolfire'
 DESCRIPTION="PhotoDissociation Region Toolbox (PDRT), astrophysics analysis tools"
 KEYWORDS = "PDR photodissociation astronomy astrophysics"
