3a3428ec358d4b51a1dab4bd812eb86d543a43d5
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-08-05 16:48:10 -0700

    changes for losangle, avperp

--- pdrtpy/test/test_modelset.py
+++ pdrtpy/test/test_modelset.py
@@ -9,21 +9,35 @@ class TestModelSet(unittest.TestCase):
         print("ModelSet Unit Test")
         success = True
         # check all models.tab files and existence of all therein
-        t = ModelSet.all_sets()
+        t = ModelSet.all_sets(debug=True)
         failed = list()
-        for n, z, md, m in zip(list(t["name"]), list(t["z"]), list(t["medium"]), list(t["mass"])):
-            print(n, z, md, m)
-            ms = ModelSet(name=n, z=z, medium=md, mass=m)
-            for r in ms.table["ratio"]:
-                try:
-                    # print(r)
-                    ms.get_model(r)
-                except Exception as e:
-                    success = False
-                    failed.append(str(e))
-            if not success:
-                print("Couldn't open these models:", failed)
-            self.assertTrue(success)
+        global_success = []
+        for n, z, md, m, losangle, avperp in zip(
+            list(t["name"]), list(t["z"]), list(t["medium"]), list(t["mass"]), list(t["losangle"]), list(t["avperp"])
+        ):
+            # print(n, z, md, m)
+            nodir = None
+            try:
+                fnf = True
+                ms = ModelSet(name=n, z=z, medium=md, mass=m, inc=losangle, avperp=avperp, debug=True)
+            except FileNotFoundError as zz:
+                success = False
+                nodir = zz.filename
+                fnf = True
+            if not fnf:
+                for r in ms.table["ratio"]:
+                    try:
+                        # print(r)
+                        ms.get_model(r)
+                    except Exception as e:
+                        success = False
+                        failed.append(str(e))
+                if not success:
+                    print("Couldn't open these models:", failed)
+            if nodir is not None:
+                print(f"Couldn't open {nodir}")
+            global_success.append(success)
+        assert all(global_success)
 
 
 if __name__ == "__main__":
