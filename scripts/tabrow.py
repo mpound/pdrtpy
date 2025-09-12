b29a0f31d7b698dda5f256d4175ab23874697b26
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-09-12 13:17:52 -0700

    add 0 angle

--- scripts/tabrow.py
+++ scripts/tabrow.py
@@ -3,7 +3,7 @@ import glob
 from astropy.io import fits
 foo="Wolfire/Kaufman        wk2020    2020 wolfirekaufman/version2020/constant_density/z=1//losangle=90/avperp=3/ models.tab constant density    1.0      "
 bar="null                                                 Wolfire/Kaufman 2020 constant density models, $A_V=7$, no freeze-out"
-for file in glob.glob( "losangle=[34679]*/*" ):
+for file in glob.glob( "losangle=[034679]*/*" ):
      #print(file)
      with fits.open(file+"/CO109_CO54.fits") as f:
         losangle=f[0].header["LOSANGLE"]
