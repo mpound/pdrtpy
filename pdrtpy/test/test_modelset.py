92e30b616edadead1a2d56d14731ebe14ea9530c
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-10-23 12:15:45 -0700

    losangle, test_add_model

--- pdrtpy/test/test_modelset.py
+++ pdrtpy/test/test_modelset.py
@@ -1,12 +1,13 @@
 # test modelset.ModelSet
 import unittest
-
 from pdrtpy.modelset import ModelSet
+from copy import deepcopy
+import pdrtpy.pdrutils as utils
 
 
 class TestModelSet(unittest.TestCase):
     def test_existence(self):
-        print("ModelSet Unit Test")
+        #print("ModelSet Unit Test")
         success = True
         # check all models.tab files and existence of all therein
         t = ModelSet.all_sets()
@@ -15,7 +16,7 @@ class TestModelSet(unittest.TestCase):
         for n, z, md, m, losangle, avperp in zip(
             list(t["name"]), list(t["z"]), list(t["medium"]), list(t["mass"]), list(t["losangle"]), list(t["avperp"])
         ):
-            # print(n, z, md, m)
+            #print(n, z, md, m, losangle, avperp)
             nodir = None
             try:
                 fnf = True
@@ -39,6 +40,19 @@ class TestModelSet(unittest.TestCase):
             global_success.append(success)
         assert all(global_success)
 
+    def test_add_model(self):
+       ms = ModelSet("wk2020", z=1)
+       a = ms.get_model("CII_158")
+       b = ms.get_model("CO_1110")
+       c = a / b
+       c.header = deepcopy(a.header)
+       c.header["TITLE"] = "[CII] 158 micron/CO (J=11-10)"
+       c.header["DATAMAX"] = c.data.max()
+       c.header["DATAMIN"] = c.data.min()
+       c.header["HISTORY"] = "Computed arithmetically from (CII_158/CO_1110)"
+       c.header["DATE"] = utils.now()  # the current time and date
+       # print(c.header)
+       ms.add_model(identifier="CII_158/CO_1110", model=c, title="[C II] 158 $\mu$m / CO(J=11-10)") 
 
 if __name__ == "__main__":
     unittest.main()
