9e623b281d3574ec054f7944a86ad07aee16154f
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-12-01 11:13:00 -0800

    add file and no-untar args

--- scripts/distribute.py
+++ scripts/distribute.py
@@ -8,30 +8,38 @@ import tarfile
 
 # for distributing Mark's FITS images from dir structure to our dir structure.
 parser = argparse.ArgumentParser(prog=sys.argv[0])
-parser.add_argument("--file",        "-f", action="store",       help="input filename", required=True)
+parser.add_argument("--file",        "-f", action="store",       help="input filename")
 parser.add_argument("--verbose",     "-v", action="store_true",  help="verbose output")
+parser.add_argument("--no-untar",    "-n", action="store_true",  help="don't untar the file (it's already untarred)")
 parser.add_argument("--dry-run",     "-d", action="store_true",  help="dry run, just print what it would do",)
 args= parser.parse_args()
 if args.dry_run:
     print(args)
-with tarfile.open(args.file,mode='r:gz') as f:
-    f.extractall(path='./00NEWMODELS')
+if not args.no_untar:
+    if args.file is None:
+        print(f"{sys.argv[0]}: error: argument --file/-f is required if untarring")
+        exit(255)
+    with tarfile.open(args.file,mode='r:gz') as f:
+        f.extractall(path='./00NEWMODELS')
 os.chdir('00NEWMODELS')
 dirs = glob.glob('LINEFITS/*')
 for d in dirs:
     angmods = glob.glob(d+"/*")
     for a in angmods:
-        avperp = f'avperp={a[-1]}'
-        losangle = f'losangle={a[-5:-3]}'
-        outdir=losangle + "/" + avperp
+        #avperp = f'avperp={a[-1]}'
+        b = a.replace("Av0","") # remove extraneous Av on some filenames
+        losangle = f'losangle={b[-2:]}'
+        outdir=losangle #+ "/" + avperp
         if args.verbose or args.dry_run:
             print(f"{a=} {outdir=}")
         if not args.dry_run:
             os.makedirs(outdir,exist_ok=True)
         fits = glob.glob(a+"/*.fits")
         for file in fits:
+            base = os.path.basename(file)
+            outfile = f"{outdir}/{base}"
             if args.verbose or args.dry_run:
-                print(f"mv {file} {outdir}")
+                print(f"cp {file} {outfile}")
             if not args.dry_run:
-                shutil.move(file,outdir)
+                shutil.copy(file,outfile)
 
