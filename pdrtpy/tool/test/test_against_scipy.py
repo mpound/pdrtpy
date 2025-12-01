0347d151d5eb7e4344c9b9ad75396e57069692fa
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-12-01 13:20:16 -0800

    clean up

--- pdrtpy/tool/test/test_against_scipy.py
+++ pdrtpy/tool/test/test_against_scipy.py
@@ -24,13 +24,14 @@ class TestExcitation:
             a.append(m)
         he = H2ExcitationFit(a)
         he.run(components=2, method='leastsq')
-        print("\n Results of pdrtpy fit:")
-        print(f'T(cold) = {he.tcold}')
-        print("T(hot) = {:>8.3f}".format(he.thot))
+        #print("\n Results of pdrtpy fit:")
+        #print(f'T(cold) = {he.tcold}')
+        #print("T(hot) = {:>8.3f}".format(he.thot))
 
         # manual fit
         clight = c.to("cm/s").value
         hplanck = h.to(u.erg*u.s).value
+        scale=hplanck*clight*1e4
 
         # Roueff et al. (2019): https://ui.adsabs.harvard.edu/abs/2019A%26A...630A..58R/abstract
         wav_H2 = {0: 28.218843793, 1: 17.034845756, 2: 12.278611991, 3: 9.664910918, 4: 8.025041036, 5: 6.909508549, 6: 6.108563840, 7: 5.511183259, 8: 5.053115155, 9: 4.694613923}
@@ -38,17 +39,17 @@ class TestExcitation:
         Tu_H2 = {2: 509.9, 3: 1015.1, 4: 1681.6, 5: 2503.7, 6: 3474.5, 7: 4586.1, 8: 5829.8, 9: 7196.7, 10: 8677.1, 11: 10261.4}
         gu_H2 = {2: 5, 3: 21, 4: 9, 5: 33, 6: 13, 7: 45, 8: 17, 9: 57, 10: 21, 11: 69}
         rotDiag_x = np.array([Tu_H2[Jl+2] for Jl in Jfluxes_pdrtpy_demo.keys()])
-        rotDiag_y = 4*np.pi * np.array([f for f in Jfluxes_pdrtpy_demo.values()]) / (hplanck*clight*1e4) * (np.array([wav_H2[Jl]/Aul_H2[Jl]/gu_H2[Jl+2] for Jl in Jfluxes_pdrtpy_demo.keys()]))
-        rotDiag_e = 4*np.pi * np.array([f for f in Jerrs_pdrtpy_demo.values()]) / (hplanck*clight*1e4) * (np.array([wav_H2[Jl]/Aul_H2[Jl]/gu_H2[Jl+2] for Jl in Jerrs_pdrtpy_demo.keys()]))
+        rotDiag_y = 4*np.pi * np.array([f for f in Jfluxes_pdrtpy_demo.values()]) / scale * (np.array([wav_H2[Jl]/Aul_H2[Jl]/gu_H2[Jl+2] for Jl in Jfluxes_pdrtpy_demo.keys()]))
+        rotDiag_e = 4*np.pi * np.array([f for f in Jerrs_pdrtpy_demo.values()]) / scale * (np.array([wav_H2[Jl]/Aul_H2[Jl]/gu_H2[Jl+2] for Jl in Jerrs_pdrtpy_demo.keys()]))
         sigma = rotDiag_e/rotDiag_y
-        fit2, err2 = curve_fit(self.func2T, rotDiag_x, np.log(rotDiag_y), p0=(1/700,20,1/200,21), bounds=(0,np.inf), max_nfev=8192, absolute_sigma=False, sigma=sigma)
-        T1fit = 1/fit2[0]
-        T1fit_err = np.sqrt(err2[0,0])/fit2[0]**2
-        T2fit = 1/fit2[2]
-        T2fit_err = np.sqrt(err2[2,2])/fit2[2]**2
-        print("\n Results of own fit:")
-        print("T(cold) = {}+-{}".format(T1fit,T1fit_err))
-        print("T(hot) = {}+-{}".format(T2fit,T2fit_err))
+        fit, err = curve_fit(self.func2T, rotDiag_x, np.log(rotDiag_y), p0=(1/700,20,1/200,21), bounds=(0,np.inf), max_nfev=8192, absolute_sigma=False, sigma=sigma)
+        T1fit = 1/fit[0]
+        T1fit_err = np.sqrt(err[0,0])/fit[0]**2
+        T2fit = 1/fit[2]
+        T2fit_err = np.sqrt(err[2,2])/fit[2]**2
+        #print("\n Results of own fit:")
+        #print("T(cold) = {}+-{}".format(T1fit,T1fit_err))
+        #print("T(hot) = {}+-{}".format(T2fit,T2fit_err))
         
         assert T1fit == pytest.approx(he.tcold.value,rel=1E-3)
         assert T2fit == pytest.approx(he.thot.value,rel=1E-3)
